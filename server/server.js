8b35a1ebebc64cb1629976cbd15d33ae099b253d
MAS <digitalcolony@gmail.com>
2018-02-14 17:20:30 -0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -3,11 +3,19 @@ const _ = require("lodash");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { ObjectID } = require("mongodb");
+const {
+  ObjectID
+} = require("mongodb");
 
-var { mongoose } = require("./db/mongoose");
-var { Todo } = require("./models/todo");
-var { User } = require("./models/user");
+var {
+  mongoose
+} = require("./db/mongoose");
+var {
+  Todo
+} = require("./models/todo");
+var {
+  User
+} = require("./models/user");
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -32,7 +40,9 @@ app.post("/todos", (req, res) => {
 app.get("/todos", (req, res) => {
   Todo.find().then(
     todos => {
-      res.send({ todos });
+      res.send({
+        todos
+      });
     },
     e => {
       res.status(400).send(e);
@@ -54,7 +64,9 @@ app.get("/todos/:id", (req, res) => {
         return res.status(404).send();
       }
 
-      res.status(200).send({ todo });
+      res.status(200).send({
+        todo
+      });
     })
     .catch(e => {
       res.status(400).send();
@@ -73,7 +85,9 @@ app.delete("/todos/:id", (req, res) => {
       if (!todo) {
         return res.status(404).send();
       }
-      res.status(200).send({ todo });
+      res.status(200).send({
+        todo
+      });
     })
     .catch(e => {
       res.status(400).send();
@@ -95,15 +109,39 @@ app.patch("/todos/:id", (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+  Todo.findByIdAndUpdate(id, {
+      $set: body
+    }, {
+      new: true
+    })
     .then(todo => {
       if (!todo) {
         return res.status(400).send();
       }
-      res.send({ todo });
+      res.send({
+        todo
+      });
     })
     .catch(e => {
       res.status(400).send();
+    });
+});
+
+// POST /users
+
+app.post("/users", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  var user = new User(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
     });
 });
 
@@ -111,4 +149,6 @@ app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
-module.exports = { app };
+module.exports = {
+  app
+};
